ffd9c6aa8e905ed0bc1dba753b546f2c45b29bbb
danchal <danchal@users.noreply.github.com>
2018-04-04 14:24:49 +0200

    now uses pipe for input file

--- setup.sh
+++ setup.sh
@@ -8,18 +8,19 @@ fi
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 SYSTEMD_UNIT="dnsmasq-log-anonymiser"
-DNSMASQ_LOG="/var/log/dnsmasq.log"
+DNSMASQ_PIPE="/var/log/dnsmasq.log.pipe"
 
 # create log file if not exist
-if [[ ! -f ${DNSMAS_LOG} ]]; then
-    touch ${DNSMASQ_LOG}
+if [[ ! -p ${DNSMASQ_PIPE} ]]; then
+    rm -f ${DNSMASQ_PIPE}
+    mkfifo ${DNSMASQ_PIPE}
 fi
 
 # do not clobber an existing config file
 cp -n ${SYSTEMD_UNIT}.conf /etc/
 
-# modify pihole dnsmasq configuration to send dnsmasq log to new log file
-sed -i -E "s:^(log-facility=).*:\1${DNSMASQ_LOG}:" /etc/dnsmasq.d/01-pihole.conf
+# modify pihole dnsmasq configuration to send dnsmasq log to new file
+sed -i -E "s:^(log-facility=).*:\1${DNSMASQ_PIPE}:" /etc/dnsmasq.d/01-pihole.conf
 
 # create systemd service
 cp ${SYSTEMD_UNIT}.service /etc/systemd/system/
